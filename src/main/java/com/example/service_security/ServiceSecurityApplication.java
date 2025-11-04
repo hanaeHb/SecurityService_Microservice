942f2496f153e32d2ee792bf7c7af9030642dac9
hanae <hanaebertili@gmail.com>
2025-11-04 22:52:20 +0100

    commit de securitySrvice

---
+++ src/main/java/com/example/service_security/ServiceSecurityApplication.java
@@ -0,0 +1,23 @@
+package com.example.service_security;
+
+import com.example.service_security.configuration.RsaKeys;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+@EnableConfigurationProperties(RsaKeys.class)
+public class ServiceSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSecurityApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/example/service_security/web/API.java
@@ -0,0 +1,105 @@
+package com.example.service_security.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+public class API {
+    private JwtEncoder jwtEncoder;
+    private JwtDecoder jwtDecoder;
+    private UserDetailsService userDetailsService;
+
+    public API(AuthenticationManager authenticationManager, JwtEncoder jwtEncoder, JwtDecoder jwtDecoder, UserDetailsService userDetailsService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+    Instant instant = Instant.now();
+    private AuthenticationManager authenticationManager;
+    @PostMapping("/login")
+    Map<String, String> login(String username, String password){
+        Map<String,String> ID_token = new HashMap<>();
+        //verifier authentification
+       Authentication authenticate = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(username, password)
+       );
+
+       //get scope
+        String Scope = authenticate.getAuthorities().stream().map(auth -> authenticate.getName()).collect(Collectors.joining(" "));
+
+       //creation des 2 id token
+       //1 - acess token
+        JwtClaimsSet jwtClaimsSet_acessToken = JwtClaimsSet.builder()
+                .subject(authenticate.getName())
+                .issuer("Security_Service")
+                .issuedAt(instant)
+                .expiresAt(instant.plus(2, ChronoUnit.MINUTES))
+                .claim("scope", Scope)
+                .build();
+
+        String Acess_Token = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet_acessToken)).getTokenValue();
+
+        //2- refresh token
+        JwtClaimsSet jwtClaimsSet_refreshToken = JwtClaimsSet.builder()
+                .subject(authenticate.getName())
+                .issuer("Security_Service")
+                .issuedAt(instant)
+                .expiresAt(instant.plus(15, ChronoUnit.MINUTES))
+                .build();
+        String refresh_Token = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet_refreshToken)).getTokenValue();
+        ID_token.put("Acecess_Token", Acess_Token);
+        ID_token.put("Refresh_token", refresh_Token);
+        return ID_token;
+    }
+    @PostMapping("/refresh")
+    public Map<String, String> refresh(String refreshToken){
+        Map<String, String> ID_Token = new HashMap<>();
+
+        if(refreshToken == null){
+            ID_Token.put("Error", "Refresh token is null" + HttpStatus.UNAUTHORIZED);
+            return ID_Token;
+        }
+
+        // verifier signature
+        Jwt decoder = jwtDecoder.decode(refreshToken);
+        String username = decoder.getSubject();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        // creation access token
+        //get scope
+        String Scope = userDetails.getAuthorities().stream().map(auth -> userDetails.getUsername()).collect(Collectors.joining(" "));
+
+        //creation des 2 id token
+        Instant instant = Instant.now();
+        //1 - acess token
+        JwtClaimsSet jwtClaimsSet_acessToken = JwtClaimsSet.builder()
+                .subject(userDetails.getUsername())
+                .issuer("Security_Service")
+                .issuedAt(instant)
+                .expiresAt(instant.plus(2, ChronoUnit.MINUTES))
+                .claim("scope", Scope)
+                .build();
+
+        String Acess_Token = jwtEncoder.encode(JwtEncoderParameters.from(jwtClaimsSet_acessToken)).getTokenValue();
+
+        ID_Token.put("Acecess_Token", Acess_Token);
+        ID_Token.put("Refresh_token", refreshToken);
+        return ID_Token;
+    }
+
+}
